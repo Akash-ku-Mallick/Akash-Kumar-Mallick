6459536f92f455b0bcb5fe265b13fbc9ec55c391
Akash-ku-Mallick <akash20m00@gmail.com>
2023-07-10 01:25:14 +0530

    Replaced Fetch with axios and adjusted the css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Skeleton,{ SkeletonTheme } from 'react-loading-skeleton'
@@ -11,132 +11,115 @@ import {
   WhatsappIcon,
   TwitterIcon
 } from "react-share";
+import axios from 'axios';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ImageComponent />
   </React.StrictMode>
 );
 
-class ImageComponent extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      imageUrl: null,
-      imageId: null,
-      shareURL: null,
-      result: null,
-      isLoading: true
-    };
+
+function ImageComponent() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [imageId, setImageId] = useState(null); 
+  const [shareURL, setShareURL] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  function handleMouseMove(event) {
+    const rect = event.currentTarget.getBoundingClientRect();
+    setTooltipPosition({ x: event.clientX - rect.left + 80, y: event.clientY - rect.top - 25 });
+  }
+
+  function getRandomAspectRatio() {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.25) {
+      return '1080/1080'; // Square
+    } else if (randomNumber < 0.5) {
+      return '1440/1080'; // Standard
+    } else if (randomNumber < 0.75) {
+      return '720/1080'; // Widescreen
+    } else {
+      return '2160:1080'; // Panoramic
+    }
+  }
+
+  const updateShareURL = (size) => {
+    axios.get(`https://picsum.photos/id/${imageId}/${size}`)
+      .then(function (response) {
+        if(response.status === 200)
+        {setShareURL(response.data.url)}
+        else {
+          console.warn("Error: ", response.status);
+        }})
+      .catch(function (error) {console.warn(error);})
   }
 
 
-  componentDidMount() {
-    this.updateImage();
-  }
-
-  updateImage = () => {
-    this.setState({isLoading: true});
-    fetch('https://picsum.photos/720/450')
-      .then((response) => {
-        this.setState({ imageUrl: response.url });
-        this.extractIdFromLink(response.url)
+  const updateImage = () => {
+    setIsLoading(true);
+    let size = getRandomAspectRatio();
+    axios
+      .get(`https://picsum.photos/${size}`)
+      .then(function (response) {
+        if(response.status === 200)
+        {
+          setImageId(response.headers["picsum-id"]);
+          setImageUrl(`https://picsum.photos/id/${imageId}/${size}`);
+          updateShareURL(size);
+        }
       })
-      .then(() => {this.setState({isLoading: false})})
-      .catch((error) => {
-        console.error('update image Error'+error);
+      .then(function () {setIsLoading(false);})
+      .catch(function (error) {
+        if (error.message.includes('Network Error')) {
+          console.warn("Network Error");
+        }
+        else {
+          console.warn("Error: ", error);
+        setTimeout(updateImage, 1000);
+        }
       });
   }
 
-  extractIdFromLink(prop) {
+  useEffect(function () {
+    updateImage();
+  }, []);
 
-    if(prop!==null) {
-      let url = prop;
-      let id = url.split('/')[4];
-
-      this.setState({imageId: id});
-      this.fetchImageUrl(id);
-    }
-    else {
-      console.log('prop----waiting......');
-      setTimeout(this.extractIdFromLink, 300);
-    }
-  }
-
-
-
-  fetchImageUrl(id) {
-    const url = `https://picsum.photos/id/${id}/info`;
-
-    const check = () => {
-      if(url!==null){
-        fetch(url)
-        .then(response => response.text())
-        .then(result => {this.setState({result: result})})
-        .then(() => {this.fetchShareUrl();})
-        .catch(error => {
-          console.error('Fetch Iamage Error Error:', error);
-        });
-      }
-      else{
-        setTimeout(check, 300);
-      }
-    }
-    check();
-  }
-
-  fetchShareUrl() {
-    const check = () => {
-      let result = this.state.result;
-      result = this.state.result;
-
-      if(result!==null){
-        var jsonObject = JSON.parse(result);
-        var url = jsonObject.url;
-        this.setState({shareURL: url});
-      }
-      else {
-        console.log('result----waiting......');
-        setTimeout(check, 300);
-      }
-    }
-    check();
-  }
-
-
-  render() {
-    
-    return (
+  return (
       <div className='container' >
         <SkeletonTheme baseColor="#595959" highlightColor="#bfbfbf" >
-          <div className="picture">
-
-            {this.state.isLoading ? <Skeleton /> :
-            <img src={this.state.imageUrl} alt="randomImage" id="image-preview" onClick={this.updateImage} />}
+          <div className="picture tooltip" onMouseMoveCapture={(event)=>{handleMouseMove(event)}}>
+          <span className="tooltiptext" style={{ left: tooltipPosition.x, top: tooltipPosition.y }}>Click on me to refresh</span>
+            {isLoading ? <Skeleton  height={'75vh'} width={'50vw'}/> :
+            <img src={imageUrl} alt="randomImage" id="image-preview" onClick={updateImage} />}
           </div>
           <div className="share">
-            <FacebookShareButton url={this.state.shareURL} >
+            {(shareURL!==null)?
+            <>
+            <FacebookShareButton title='Click to Share' url={imageUrl} >
               <FacebookIcon size={48} round={false} />
             </FacebookShareButton>
-            <TwitterShareButton url={this.state.shareURL} >
+            </>:<Skeleton height={48} width={48} />}
+            {(shareURL!==null)?
+            <>
+            <TwitterShareButton title='Click to Share' url={shareURL} >
               <TwitterIcon size={48} round={false} />
             </TwitterShareButton>
-            <WhatsappShareButton url={this.state.shareURL} >
-              <WhatsappIcon size={48} round={false} />
+            </>:<Skeleton height={48} width={48} />}
+            {(shareURL!==null)?
+            <>
+            <WhatsappShareButton title='Click to Share' url={shareURL} >
+              <WhatsappIcon size={48} round={false}/>
             </WhatsappShareButton>
+            </>
+            :<Skeleton height={48} width={48} />}
           </div>
         </SkeletonTheme>
       </div>
     )
-  }
-
 }
 
-function App() {
-  return (
-    <ImageComponent />
-  );
-}
